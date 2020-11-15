cb33fb6c1e717e0b0da236eabe0e26959054694a
ilan ledder <ledderi@hotmail.com>
2020-11-15 17:17:28 +0200

    Initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            Product product = await _context.Products.FindAsync(id);
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+    }
+}
