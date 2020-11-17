5fc19701818715d0743059ef8097807c4051d50a
ilan ledder <ledderi@hotmail.com>
2020-11-17 14:06:16 +0200

    end of section #3

--- Infrastructure/Data/ProductsRepository.cs
+++ Infrastructure/Data/ProductsRepository.cs
@@ -20,13 +20,20 @@ namespace Infrastructure.Data
 
         public async Task<Product> GetProductAsync(int id)
         {
-            Product product = await _context.Products.FindAsync(id);
+            Product product = await _context.Products
+                .Include(p => p.ProductBrand)
+                .Include(p => p.ProductType)
+                .SingleOrDefaultAsync(p => p.Id == id);                
+                
             return product;
         }
 
         public async Task<IEnumerable<Product>> GetProductsAsync()
         {
-            IEnumerable<Product> products = await _context.Products.ToArrayAsync();
+            IEnumerable<Product> products = await _context.Products
+                .Include(p => p.ProductBrand)
+                .Include(p => p.ProductType)
+                .ToArrayAsync();
             return products;
         }
     }
